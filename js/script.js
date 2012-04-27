3c2f91183d774b8381af9e0e880e86b0da227eb9
Lucky Sebastian <luckyrps@yahoo.com>
2012-04-27 09:49:14 +0800

    added footer and font

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@
 */
 
 $(function() {
+
 	$('div.account a.toggle').click(function(){
 		$('div.account').toggleClass('active');
-		$('div.account a.toggle span.person').toggleClass('hide');
 		$('div.account ul').toggleClass('hide');
 	});
-
 	
 });
