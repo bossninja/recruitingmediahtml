0fb721865213957e686a121941a80b5c888b2142
Lucky Sebastian <luckyrps@yahoo.com>
2012-05-07 00:15:01 +0800

    expand archive function implemented

--- js/script.js
+++ js/script.js
@@ -12,5 +12,13 @@ $(function() {
 	$('.sub-navigation li a').hover(function() {
 		$(this).find('span').toggleClass('active');
 	});
+
+	$('.archives a.toggle').click(function() {
+		var txt = $(this).find('span').text() == '\u002B Expand' ? '\u002D Collapse' : '\u002B Expand';
+		$(this).find('span').text(txt);
+		
+		$('.archives ul').toggleClass('hide');
+		return false;
+	});
 	
 });
