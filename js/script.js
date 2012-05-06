f7416da93f2fa6a6a02d6737585793095d0c4f9d
Lucky Sebastian <luckyrps@yahoo.com>
2012-05-06 22:05:36 +0800

    arrow should be gray when hover

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@
 
 $(function() {
 
-	$('div.account a.toggle').click(function(){
+	$('div.account a.toggle').click(function() {
 		$('div.account').toggleClass('active');
 		$('div.account ul').toggleClass('hide');
+	});
+
+	$('.sub-navigation li a').hover(function() {
+		$(this).find('span').toggleClass('active');
 	});
 	
 });
